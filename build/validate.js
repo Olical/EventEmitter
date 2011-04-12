0e334c01b5cce1da8cd522f90820ce06197d1b3a
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-12 19:36:11 +0100

    Added the build tools, taken from Spark

---
+++ build/validate.js
@@ -0,0 +1,25 @@
+// Initialisation
+var src = require('fs').readFileSync('spark.js', 'utf8'),
+	sys = require('sys'),
+	jshint = require('./jshint').JSHINT,
+	i = null,
+	e = null;
+
+// Run jshint
+var result = jshint(src, {
+	curly: true,
+	eqeqeq: true,
+	forin: true,
+	browser: true,
+	evil: true
+});
+
+// Check for errors
+if(!result) {
+	// It's the end of the world!
+	for(i = 0; i < jshint.errors.length; i++) {
+		// Log the error
+		e = jshint.errors[i];
+		sys.puts(e.raw + ' (' + e.line + ':' + e.character + ')' + "\n" + e.evidence + "\n");
+	}
+}
