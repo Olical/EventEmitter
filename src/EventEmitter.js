294c7bca7ba62833c0ce2af7a99c947b9f81945e
Oliver Caldwell <oliver@flowdev.co.uk>
2011-06-24 22:27:32 +0100

    Created the addListener function.

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -7,5 +7,29 @@
  */
 
 function EventEmitter() {
+	// Put the instance in scope and initialise required variables
+	var instance = this,
+		events = {};
 	
+	/**
+	 * Assigns a listener to the specified event
+	 * 
+	 * @param {String} eventName Name of the event to assign the listener to
+	 * @param {Function} listener Function to be executed when the specified event is emitted
+	 * @returns {Object} The current instance of EventEmitter to allow chaining
+	 */
+	instance.addListener = function(eventName, listener) {
+		// Check if we currently have a listener array for the specified event
+		if(typeof events[eventName] === 'undefined') {
+			// We do not, create it with our listener inside
+			events[eventName] = [listener];
+		}
+		else {
+			// We do, push the listener onto the end
+			events[eventName].push(listener);
+		}
+		
+		// Return the instance to allow chaining
+		return instance;
+	};
 }
