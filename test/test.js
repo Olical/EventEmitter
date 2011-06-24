24c1ea458d033631bca531505106795bfc589d91
Oliver Caldwell <oliver@flowdev.co.uk>
2011-06-24 21:35:40 +0100

    Added more tests, including the once test.

--- test/test.js
+++ test/test.js
@@ -5,17 +5,36 @@
 (function() {
 	var ee = new EventEmitter();
 	
-	module('addListener');
-	
-	test('Adding a listener with a string for a name', function() {
+	test('Adding a listener', function() {
 		equals(ee, ee.addListener('test1', function() {
+			return true;
+		}), 'addListener should return the instance to allow chaining.');
+		
+		equals(ee, ee.addListener(10, function() {
 			return true;
 		}), 'addListener should return the instance to allow chaining.');
 	});
 	
-	test('Adding a listener with a number for a name', function() {
-		equals(ee, ee.addListener(1337, function() {
-			return true;
+	test('Emitting an event', function() {
+		var checkThis = false;
+		
+		equals(ee, ee.addListener('test2', function() {
+			checkThis = true;
 		}), 'addListener should return the instance to allow chaining.');
+		equals(ee, ee.emit('test2'), 'emit should return the instance to allow chaining.');
+		equals(true, checkThis, 'checkThis should equal true.');
+	});
+	
+	test('Adding a listener that will only run once', function() {
+		var checkThis = 0;
+		
+		equals(ee, ee.once('test3', function() {
+			checkThis += 1;
+		}), 'once should return the instance to allow chaining.');
+		equals(ee, ee.emit('test3'), 'emit should return the instance to allow chaining.');
+		ee.emit('test3');
+		ee.emit('test3');
+		ee.emit('test3');
+		equals(1, checkThis, 'checkThis should equal 1 after multiple emittions.');
 	});
 }());
