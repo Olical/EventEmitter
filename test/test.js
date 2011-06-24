3a71a51705acc1ce35c7c4ce9829de4ecef83874
Oliver Caldwell <oliver@flowdev.co.uk>
2011-06-24 21:18:20 +0100

    Added a couple of tests for addListener

--- test/test.js
+++ test/test.js
@@ -3,5 +3,19 @@
  */
 
 (function() {
+	var ee = new EventEmitter();
 	
+	module('addListener');
+	
+	test('Adding a listener with a string for a name', function() {
+		equals(ee, ee.addListener('test1', function() {
+			return true;
+		}), 'addListener should return the instance to allow chaining.');
+	});
+	
+	test('Adding a listener with a number for a name', function() {
+		equals(ee, ee.addListener(1337, function() {
+			return true;
+		}), 'addListener should return the instance to allow chaining.');
+	});
 }());
