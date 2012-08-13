77943eb45a8e9ce35ff55a063e28d26635d0ac6e
Oliver Caldwell <olliec87@gmail.com>
2012-08-13 22:45:35 +0100

    Added the basic init code for the class. Initialises the class and it's prototype shortcut, also exposes the class via AMD or the global object.

--- EventEmitter.js
+++ EventEmitter.js
@@ -5,4 +5,29 @@
  * Released under the MIT license - http://opensource.org/licenses/MIT
  */
 
-;(function(){}());
+;(function(exports) {
+    /**
+     * Class for managing events.
+     * Can be extended to provide event functionality in other classes.
+     *
+     * @class Manages event registering and emitting.
+     */
+    function EventEmitter(){}
+
+    /**
+     * Provides a shortcut to the prototype.
+     *
+     * @property {object} Shortcut to the prototype object.
+     */
+    EventEmitter.fn = EventEmitter.prototype;
+
+    // Expose the class either via AMD or the global object
+    if(typeof define === 'function' && define.amd) {
+        define(function() {
+            return EventEmitter;
+        });
+    }
+    else {
+        exports.EventEmitter = EventEmitter;
+    }
+}(this));
