5b7ee4b5686a047567db7836d794672b407c38c9
Oliver Caldwell <olliec87@gmail.com>
2012-08-13 22:07:44 +0100

    Added the basic source file. Contains the name, version, source URL, copyright and license.

---
+++ EventEmitter.js
@@ -0,0 +1,8 @@
+/**
+ * EventEmitter v4.0.0 - https://github.com/Wolfy87/EventEmitter
+ *
+ * Copyright 2012 Oliver Caldwell - http://oli.me.uk/
+ * Released under the MIT license - http://opensource.org/licenses/MIT
+ */
+
+;(function(){}());
