81b11db6da152d49363afc02d2d56a28f0d5551a
Oliver Caldwell <olliec87@gmail.com>
2012-08-22 21:46:25 +0100

    Removed the tests of the now private indexOfListener function.

--- tests/tests.js
+++ tests/tests.js
@@ -41,32 +41,6 @@ describe('EventEmitter.fn.getListeners', function() {
     });
 });
 
-describe('EventEmitter.fn.indexOfListener', function() {
-    var ee,
-        fn = function(){};
-
-    beforeEach(function() {
-        ee = new EventEmitter();
-    });
-
-    it('returns -1 on non existent listeners', function() {
-        expect(ee.indexOfListener(fn, ee.getListeners('foo'))).toEqual(-1);
-
-        // Testing again to make sure it does not work on init of storage only
-        expect(ee.indexOfListener(fn, ee.getListeners('foo'))).toEqual(-1);
-    });
-
-    it('returns the index of an existing listener', function() {
-        var listeners = ee.getListeners('foo');
-        listeners.push('a');
-        listeners.push('b');
-        listeners.push(fn);
-        listeners.push('c');
-
-        expect(ee.indexOfListener(fn, listeners)).toEqual(2);
-    });
-});
-
 describe('EventEmitter.fn.addListener', function() {
     var ee,
         fn1 = function(){},
