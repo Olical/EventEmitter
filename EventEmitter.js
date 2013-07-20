e6946864f92ab5fc037bcb236384dbb48f29999a
David DeSandro <desandrocodes@gmail.com>
2013-07-20 16:47:31 -0400

    simplify removing listener.listener

--- EventEmitter.js
+++ EventEmitter.js
@@ -355,7 +355,7 @@
 					listener = listeners[key][i];
 					response = listener.listener.apply(this, args || []);
 					if (response === this._getOnceReturnValue() || listener.once === true) {
-						this.removeListener(evt, listeners[key][i].listener);
+						this.removeListener(evt, listener.listener);
 					}
 				}
 			}
