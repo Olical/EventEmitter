ecac6225c52b3876761d690cb139842ac316fbbe
Oliver Caldwell <olliec87@gmail.com>
2013-08-05 17:39:14 +0100

    Joined the alias JSDoc into one line.
    I prefer the 80 character limit, but again, I think it will mess with
    the generated documentation.

--- EventEmitter.js
+++ EventEmitter.js
@@ -42,8 +42,7 @@
 	}
 
 	/**
-	 * Alias a method while keeping the context correct, to allow for overwriting
-	 * of target method.
+	 * Alias a method while keeping the context correct, to allow for overwriting of target method.
 	 *
 	 * @param {String} name The name of the target method.
 	 * @return {Function} The aliased method
