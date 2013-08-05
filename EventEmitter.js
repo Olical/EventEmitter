f24fc1d6b15d50e3e57e6cdbe67d05139754e4c0
Oliver Caldwell <olliec87@gmail.com>
2013-08-05 17:38:25 +0100

    Removed some spaces in the alias JSDoc.
    It makes the comment look a lot nicer, but it might mess up the
    generated documentation. I'm just keeping it the same as all of the
    other JSDoc strings for now.

--- EventEmitter.js
+++ EventEmitter.js
@@ -45,8 +45,8 @@
 	 * Alias a method while keeping the context correct, to allow for overwriting
 	 * of target method.
 	 *
-	 * @param  {String} name The name of the target method.
-	 * @return {Function}    The aliased method
+	 * @param {String} name The name of the target method.
+	 * @return {Function} The aliased method
 	 */
 	function alias(name) {
 		return function() {
